3214c62930522ccb70ab4734ed22e8f39275518c
Amit Ghosh <29959138+maverickamit@users.noreply.github.com>
2019-03-22 12:07:07 +0530

    Update myscript.js

--- myscript.js
+++ myscript.js
@@ -19,37 +19,17 @@ $(".progress").each(function() {
     .html(data + "%");
 });
 
-!(function(t) {
-  t.fn.bcSwipe = function(e) {
-    var n = { threshold: 50 };
-    return (
-      e && t.extend(n, e),
-      this.each(function() {
-        function e(t) {
-          1 == t.touches.length &&
-            ((u = t.touches[0].pageX),
-            (c = !0),
-            this.addEventListener("touchmove", o, !1));
-        }
-        function o(e) {
-          if (c) {
-            var o = e.touches[0].pageX,
-              i = u - o;
-            Math.abs(i) >= n.threshold &&
-              (h(), t(this).carousel(i > 0 ? "next" : "prev"));
-          }
-        }
-        function h() {
-          this.removeEventListener("touchmove", o), (u = null), (c = !1);
-        }
-        var u,
-          c = !1;
-        "ontouchstart" in document.documentElement &&
-          this.addEventListener("touchstart", e, !1);
-      }),
-      this
-    );
-  };
-})(jQuery);
-
-$(".carousel").bcSwipe({ threshold: 50 });
+$(".carousel").on("touchstart", function(event) {
+  var xClick = event.originalEvent.touches[0].pageX;
+  $(this).one("touchmove", function(event) {
+    var xMove = event.originalEvent.touches[0].pageX;
+    if (Math.floor(xClick - xMove) > 5) {
+      $(this).carousel("next");
+    } else if (Math.floor(xClick - xMove) < -5) {
+      $(this).carousel("prev");
+    }
+  });
+  $(".carousel").on("touchend", function() {
+    $(this).off("touchmove");
+  });
+});
